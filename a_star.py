9fae04419ec95163aaa6611ba3bea20f39b20bf4
ataniel <10ataniel@gmail.com>
2025-12-22 12:30:43 -0300

    a star updated

--- a_star.py
+++ a_star.py
@@ -1,7 +1,10 @@
 from env.MazeEnv import *
 from models.AStar import a_star_maze_solve 
 
-env = MazeEnv("malo.maze")
+env = MazeEnv("../C/c-qlearning/gwyn_1.maze")
+
+print(f"{env.agent_start} --> {env.agent_goal}")
+print(f"Walls count: {env.walls_count}")
 
 path = a_star_maze_solve(env)
 
